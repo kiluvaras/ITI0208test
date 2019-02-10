e5b73329ba587abf3fd40246ef6dea3eb4aec621
Priit <prpall@ttu.ee>
2019-02-10 14:04:21 +0200

    done

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,15 +8,6 @@ public class DemoController {
 
     @GetMapping("hello")
     public String hello() {
-//        return "Hello stranger!";
-        return "index";
+        return "Hello stranger!";
     }
-
-//    @RequestMapping(value = "/", method = RequestMethod.GET)
-//    public String index(Model model) {
-        // this attribute will be available in the view jama.html as a thymeleaf variable
-//        model.addAttribute("eventName", "FIFA 2018");
-        // this just means render jama.html from static/ area
-//        return "index";
-//    }
 }
